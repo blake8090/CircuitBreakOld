cd591f60fe722d1bdb589e9a6ae121b200a45eca
Blake Edwards <blake8090@gmail.com>
2016-09-19 16:26:16 -0400

    Added variable jump height, fixed jump spam

--- scripts/character.gd
+++ scripts/character.gd
@@ -9,8 +9,10 @@ var GRAVITY
 
 var velocity = Vector2()
 var can_wall_jump = true
+var can_jump = true
 var is_jumping = false
 var is_falling = false
+var jump_pressed = false
 
 func _fixed_process(delta):
 	if test_move(Vector2(0,1)) && velocity.y > 0:
@@ -22,30 +24,41 @@ func _fixed_process(delta):
 	else:
 		GRAVITY = 800
 		is_falling = true
-		
+
 	# reset y velocity if char hit ceiling
 	if test_move(Vector2(0,-1)):
 		velocity.y = 0
-	
+
+	# force player to release button in order to jump again
+	if not is_falling and jump_pressed:
+		can_jump = false
+
 	# double checking left and right avoids buggy movement 
 	# along y-axis when pressing against walls
 	var can_move_left = true
 	if is_falling and test_move(Vector2(-1,0)):
 		can_move_left = false
-		
+
 	var can_move_right = true
 	if is_falling and test_move(Vector2(1,0)):
 		can_move_right = false
-	
+
 	if Input.is_action_pressed("ui_left") and can_move_left:
 		# kill velocity if pressing against an opposing wall for faster response time
 		if test_move(Vector2(1,-1)): velocity.x = 0
-		velocity.x -= ACCELERATION
+		# ensure quick direction switching
+		if velocity.x > 0: 
+			velocity.x -= ACCELERATION + FRICTION
+		else: 
+			velocity.x -= ACCELERATION
 		# recalculate gravity for current x-velocity to avoid sliding down slopes at low speeds
 		if not is_jumping and not is_falling: GRAVITY = calc_gravity_on_slope(abs(velocity.x))
 	elif Input.is_action_pressed("ui_right") and can_move_right:
 		if test_move(Vector2(-1,-1)): velocity.x = 0
-		velocity.x += ACCELERATION
+		if velocity.x < 0: 
+			velocity.x += ACCELERATION + FRICTION
+		else: 
+			velocity.x += ACCELERATION
 		if not is_jumping and not is_falling: GRAVITY = calc_gravity_on_slope(abs(velocity.x))
 	else:
 		# apply friction if not moving (unless we hit a wall, then stop immediately)
@@ -58,44 +71,53 @@ func _fixed_process(delta):
 			elif velocity.x > 0:
 				velocity.x -= FRICTION
 				if velocity.x < 0: velocity.x = 0
-	
+
 	if abs(velocity.x) > MAX_SPEED:
 		if velocity.x < 0: velocity.x = -MAX_SPEED
 		if velocity.x > 0: velocity.x = MAX_SPEED
 	
-	get_node("../Label").set_text(str(velocity.x))
+	get_node("../Label").set_text(str(velocity.y))
 	
 	velocity.y += delta * GRAVITY
-	
-	if Input.is_action_pressed("ui_up") and not is_falling:
-		velocity.y = -JUMP_HEIGHT
-		is_jumping = true
-	elif Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_left"):
-		if is_falling and not can_move_left and can_wall_jump:
-			pass
-	
+
+	if Input.is_action_pressed("ui_up"):
+		jump_pressed = true
+		if not is_falling and can_jump:
+			is_jumping = true
+			velocity.y = -JUMP_HEIGHT
+	#elif Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_left"):
+		#jump_pressed = true
+		#if is_falling and not can_move_left and can_wall_jump:
+		#	pass
+	else:
+		if jump_pressed: # jump button has been released 
+			if is_jumping and velocity.y < -100:
+				velocity.y = -100
+			jump_pressed = false
+			can_jump = true
+
 	var motion = velocity * delta
-	
+
 	# record old x pos and velocity. setting the x pos to the old 
 	# x pos after moving (if there was no x-axis movement in the
 	# first place) avoids gravity affecting x-axis on slopes
 	var old_x = get_global_pos().x;
 	var old_velocity_x = velocity.x;
 	motion = move(motion)
-	
+
 	if is_colliding():
 		# avoid slow movement on slopes
 		var n = get_collision_normal()
 		motion = n.slide(motion).normalized() * abs(motion.x)
 		move(motion)
-	
+
 	# reset x pos if there was no movement on x-axis
 	if old_velocity_x == 0:
 		set_global_pos(Vector2(old_x,get_global_pos().y))
-	
+
 func calc_gravity_on_slope(speed):
 	# best ratio for 45 degree slopes is 10000 gravity per 200 speed
 	return (10000 * speed) / 200
-	
+
 func _ready():
 	set_fixed_process(true)
