c136dd76039c771ee1e28299d31111fa8c279de6
Blake Edwards <blake8090@gmail.com>
2016-09-22 18:19:08 -0400

    Fix potential null object runtime error
    Bullet now stores creator object's name at initialization. Protects
    against cases such as when an object creates a bullet and is destroyed
    before the bullet hits an object

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -3,12 +3,15 @@ extends Area2D
 
 var velocity = Vector2(0,0)
 var shooter
+var shooter_name = ""
 
 func _ready():
 	set_process(true)
 
 func init(shooter, velocity):
 	self.shooter = shooter
+	# get name now to protect against cases where shooter object has been deleted before querying
+	shooter_name = shooter.get_name()
 	self.velocity = velocity 
 
 func _process(delta):
@@ -23,7 +26,7 @@ func _on_bullet_area_enter(area):
 	hit_object(area)
 
 func hit_object(obj):
-	if obj.get_name() != shooter.get_name():
+	if obj.get_name() != shooter_name:
 		# check if colliding with instance of self
 		if not obj.has_method("is_bullet"):
 			if obj.has_method("hit"):
