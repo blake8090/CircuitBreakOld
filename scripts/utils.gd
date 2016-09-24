0c9652e96697c393b58af0c624fc4173b41ba925
Blake Edwards <blake8090@gmail.com>
2016-09-23 20:31:51 -0400

    Added generic utils script

---
+++ scripts/utils.gd
@@ -0,0 +1,9 @@
+
+extends Node
+
+static func get_velocity_from_angle(angle, speed):
+	var angleRad = (angle * PI) / 180
+	var velocity = Vector2(0,0)
+	velocity.x -= sin(angleRad) * speed
+	velocity.y -= cos(angleRad) * speed
+	return velocity
