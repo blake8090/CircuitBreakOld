8c806bf1c1f5bd62745310266c94c38220fb2bca
Blake Edwards <blake8090@gmail.com>
2016-09-27 14:46:29 -0400

    added better animation handling to character.gd

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,21 +1,29 @@
 
 extends "ground_actor.gd"
 
+const Utils = preload("utils.gd")
+
 var root = null
 onready var sprite = get_node("Sprite")
 onready var health_node = get_node("health")
+onready var anim_player = get_node("AnimationPlayer")
+
+var _shoot_anim_map = {           # List of looped animations (like running and idling) and their corresponding shooting animations.
+	idle = "idle_shoot",          # If a looped anim does not have a corresponding shooting anim, no anim switching will occur
+	run  = ""
+}
+var _current_shoot_anim = ""      # Stores the current shooting animation as our looped animations might change in the future. If we
+                                  # directly used the list instead, checking to see if it's done playing would be inaccurate
+var _current_looped_anim = "idle" # Used to reset animation to what was originally playing after finishing one-shot animations like shoot
+var _play_shoot_anim = false
 
 func _fixed_process(delta):
 	root.get_node("HUDLayer/player_health").set_text("Player Health: " + str(health_node.health))
 	
-	# if done playing the shoot anim then play the idle anim
-	if get_node("AnimationPlayer").get_current_animation() == "idle_shoot" and not get_node("AnimationPlayer").is_playing():
-		get_node("AnimationPlayer").play("idle")
-
 	if Input.is_action_pressed("ui_left"):
 		action_move_left = true
 		action_move_right = false
-		
+		_current_looped_anim = "run"
 		if _facing_right:
 			sprite.set_flip_h(true)
 			# flip shoot_pos to match sprite
@@ -24,7 +32,7 @@ func _fixed_process(delta):
 	elif Input.is_action_pressed("ui_right"):
 		action_move_right = true
 		action_move_left = false
-		
+		_current_looped_anim = "run"
 		if not _facing_right:
 			sprite.set_flip_h(false)
 			# flip shoot_pos to match sprite
@@ -34,9 +42,36 @@ func _fixed_process(delta):
 		# halt movement
 		action_move_left = false
 		action_move_right = false
+		_current_looped_anim = "idle"
 	
 	if Input.is_action_pressed("ui_up"):
 		action_jump = true
+	
+	_handle_animation()
+
+func _anim_shoot():
+	if _shoot_anim_map[_current_looped_anim] != "":
+		_play_shoot_anim = true
+		_current_shoot_anim = _shoot_anim_map[_current_looped_anim]
+		anim_player.play(_current_shoot_anim)
+
+func _handle_animation():
+	if _play_shoot_anim:
+		# reset animation if shoot animation has finished
+		if anim_player.get_current_animation() == _current_shoot_anim and not anim_player.is_playing():
+			anim_player.play(_current_looped_anim)
+			_play_shoot_anim = false
+	else:
+		# update looped animation
+		if anim_player.get_current_animation() != _current_looped_anim:
+			anim_player.play(_current_looped_anim)
+		
+		# scale running animation speed to horizontal velocity if playing
+		if anim_player.get_current_animation() == "run":
+			var speed = abs(Utils.scale_to_range(0, max_speed, _velocity.x))
+			# limit playing speed to base value above 0 to ensure fluid motion at low speeds
+			if speed < 0.3: speed = 0.3
+			anim_player.set_speed(speed)
 
 func hit(projectile, damage):
 	var p = ObjectFactory.create_fx_explosion(get_global_pos())
@@ -57,7 +92,7 @@ func _death(projectile):
 
 func _input(event):
 	if event.is_action("shoot") and not event.is_echo() and event.is_pressed():
-		get_node("AnimationPlayer").play("idle_shoot")
+		_anim_shoot()
 		var speed
 		if _facing_right: speed = Vector2(1,0) * 500
 		else: speed = Vector2(-1,0) * 500
